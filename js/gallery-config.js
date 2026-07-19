9612cfe3c53e15c1762d5865c460e9c584e47b05
fyriz20-design <fyriz20@gmail.com>
2026-07-19 17:05:19 +0200

    Blumen-Deko-Bild eingebunden

--- js/gallery-config.js
+++ js/gallery-config.js
@@ -12,5 +12,5 @@ const GALLERY_IMAGES = [
   { src: 'images/WhatsApp Image 2026-05-26 at 14.18.34 (4).jpeg', label: 'Hochzeit'  },
   { src: 'images/WhatsApp Image 2026-05-26 at 14.18.34 (6).jpeg', label: 'Dekoration'},
   { src: 'images/Geburtstag.jpg', label: 'Geburtstag' },
-  { src: 'images/blumen-deko.jpg', label: 'Blumen' },
+  { src: 'images/blumen-deko.jfif', label: 'Blumen' },
 ];
